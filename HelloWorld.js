a6cb6c32a2cc6732fc864f17e9f7d4a138435cd2
surabhi8 <surabhigupta2442@gmail.com>
2018-01-16 21:35:56 +0530

    Hello World Example done successfully

---
+++ HelloWorld.js
@@ -0,0 +1,5 @@
+function upperCaser(input){
+   return input.toUpperCase();;
+}
+module.exports = upperCaser;
+console.log("The function converts the string to uppercase?",upperCaser("surabhi")==="SURABHI")
