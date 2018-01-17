9e5f57757ad8ad5dc484e6d6351512bae13f6516
surabhi8 <surabhigupta2442@gmail.com>
2018-01-17 13:45:54 +0530

    Recursion done

---
+++ duck.js
@@ -0,0 +1,21 @@
+function duckCount() {
+  return Array.prototype.filter.call(arguments,(function(object){
+    return Object.prototype.hasOwnProperty.call(object, 'quack');
+  })).length;
+}
+module.exports = duckCount
+{
+  let notDuck = Object.create({quack: true})
+  let duck = {quack: true}
+  console.log("The number of objects that has quack property is?",duckCount(duck, notDuck)===1)
+}
+
+{
+
+  console.log("The number of objects that has quack property is?",duckCount()===0)
+}
+{
+  let duck1 = {quack: true}
+  let duck2 = {quack: true}
+  console.log("The number of objects that has quack property is?",duckCount(duck1,duck2)===2)
+}
